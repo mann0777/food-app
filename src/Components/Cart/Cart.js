3cc54db92326d32eca005007454eb6c344dbe6e0
mann0777 <mannamloh77@gmail.com>
2022-08-26 11:08:40 +0530

    using effect hook

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
@@ -6,26 +7,29 @@ import CartContext from '../../Store/cart-context';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`; 
-  const hasItems = cartCtx.items.length>0;
 
-  const cartItemRemoveHandler=(id)=>{
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
   };
 
-  const cartItemAddHandler=(item)=>{
-    cartCtx.addItem({...item, amount:1});
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
   };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
-        <CartItem key= {item.id} 
-        name={item.name} 
-        amount={item.amount} 
-        price={item.price} 
-        onRemove={cartItemRemoveHandler.bind(null, item.id)} 
-        onAdd={cartItemAddHandler.bind(null, item.id)}/> 
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -38,7 +42,9 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
         {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
